2267f3c6c2da16fa498695eba965f8ee63e7ddbd
Eric Huang <ehuang@zendesk.com>
2015-02-19 17:46:53 -0800

    Initial Commit

---
+++ tweet_yo_self/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tweet_yo_self
+//
+//  Created by Eric Huang on 2/19/15.
+//  Copyright (c) 2015 Eric Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
